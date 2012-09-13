60be112f7cd579d42c7f44ba705f56fa1e26ee85
Mohit Thatte <mohit.thatte@gmail.com>
2012-09-13 23:02:02 +0530

    User email must be unique

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe User do
-  it { should allow_mass_assignment_of :name }
-  it { should allow_mass_assignment_of :email }
-  it { should allow_mass_assignment_of :password }
-  it { should allow_mass_assignment_of :password_confirmation }
+  describe 'attr_accessible' do
+    it { should allow_mass_assignment_of :name }
+    it { should allow_mass_assignment_of :email }
+    it { should allow_mass_assignment_of :password }
+    it { should allow_mass_assignment_of :password_confirmation }
+  end
+  describe 'validations' do
+    it { should validate_uniqueness_of :email }
+  end
 end
